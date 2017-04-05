3c78bbf534533c2ac4f2fc46d0fe0d2cb3b20352
Urvesh Tanna <urvesh.tanna@truce.in>
2017-04-05 11:23:43 +0530

    minor changes

--- index.js
+++ index.js
@@ -15,17 +15,16 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.send('Hello world!')
 })
 
-// for Facebook verification
+// for get request
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
+  console.log(req);
+  res.send(req);
 })
 
+// for POST request
 app.post('/webhook/', function (req, res) {
     if(req && req.body && req.body.order_id){
       var text = req.body.order_id;
